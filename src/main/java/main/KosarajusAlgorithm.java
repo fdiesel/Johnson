0b2df57c2386de52d670b773031f87d1d4dfc179
Florian Diesel <florian.diesel@posteo.de>
2021-01-04 14:42:23 +0100

    Replaced visited array by hashset

--- src/main/java/main/KosarajusAlgorithm.java
+++ src/main/java/main/KosarajusAlgorithm.java
@@ -15,43 +15,46 @@ public class KosarajusAlgorithm {
 	// stores the vertices in order of finishing depth search
 	Deque<Integer> stack;
 	// stores info if vertex has already been visited
-	boolean[] visited;
+	Set<Integer> visited;
 	// list of strongly connected components found by algorithm
 	List<Set<Integer>> result = new ArrayList<>();
 	// stores info if algorithm is in second part (changes a bit of depthSearch)
 	boolean afterReverse;
 
 	public List<DirectedGraph<Integer>> findStronglyConnectedComponents(DirectedGraph<Integer> inputGraph) {
+
 		// clone graph to not modify inputGraph
 		DirectedGraph<Integer> graph = inputGraph.clone();
 		result.clear();
 		afterReverse = false;
 		int vertices = graph.getVertexCount();
-		visited = new boolean[vertices];
+		visited = new HashSet<>();
 		stack = new ArrayDeque<>();
+
 		// depthSearch on all vertices not visited
-		for (int i = 0; i != vertices; i++) {
-			if (!visited[i]) {
-				depthSearch(i, graph, null); // null input because not needed before (afterReverse = true)
+		for (Vertex<Integer> vertex : graph.getAllVertices()) {
+			if (!visited.contains(vertex.getId())) {
+				// null input because not needed before (afterReverse = true)
+				depthSearch(vertex.getId(), graph, null);
 			}
 		}
+
 		// reverse graph, second part of algorithm begins
 		graph = graph.reverse();
 		afterReverse = true; // Changes the functionality of depthSearch
 		// set all vertices to not visited
-		visited = new boolean[vertices];
+		visited.clear();
 
 		// depthSearch on all not visited Elements of stack in order
 		while (!stack.isEmpty()) {
 			int current = stack.removeLast();
-			if (!visited[(int) current]) {
+			if (!visited.contains(current)) {
 				Set<Integer> set = new HashSet<>();
 				depthSearch(current, graph, set);
 				result.add(set);
-				System.out.println(set); // temporary way to access strongly connected components till subGraph is fixed
 			}
 		}
-		// subGraph routine is not working correctly (error here or in Graph.java)
+
 		List<DirectedGraph<Integer>> subGraphs = new ArrayList<DirectedGraph<Integer>>();
 
 		result.forEach(set -> subGraphs.add(inputGraph.getSubGraph(set)));
@@ -60,14 +63,14 @@ public class KosarajusAlgorithm {
 	}
 
 	public void depthSearch(int vertexId, DirectedGraph<Integer> graph, Set<Integer> set) {
-		visited[(int) vertexId] = true;
+		visited.add(vertexId);
 		if (afterReverse)
 			set.add(vertexId); // if in part 2 of algo build strongly connected component in set
 		// for each adjacent vertex not visited do depthSearch (= recursive
 		// implementation of depthSearch)
 		for (Vertex<Integer> current : graph.getVertex(vertexId).getAdjacentVertices()) {
-			int currentID = (int) current.getId();
-			if (!visited[currentID]) {
+			int currentID = current.getId();
+			if (!visited.contains(currentID)) {
 				depthSearch(currentID, graph, set);
 			}
 		}
