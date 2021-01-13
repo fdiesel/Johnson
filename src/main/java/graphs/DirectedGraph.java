5b3df6ef6fe0677d683cb64b91ffb829b05a22d3
Florian Diesel <florian.diesel@posteo.de>
2021-01-13 03:15:55 +0100

    javadoc and optimization

--- src/main/java/graphs/DirectedGraph.java
+++ src/main/java/graphs/DirectedGraph.java
@@ -18,10 +18,18 @@ public class DirectedGraph<T> implements Cloneable {
 
 	private Map<Integer, Vertex<T>> allVertices;
 
+	/**
+	 * Creates a new DirectedGraph
+	 */
 	public DirectedGraph() {
 		allVertices = new HashMap<Integer, Vertex<T>>();
 	}
 
+	/**
+	 * Adds a Vertex to the Graph, if it doesn't contain the Vertex yet
+	 * 
+	 * @param vertex to add
+	 */
 	public void addVertex(Vertex<T> vertex) {
 		if (allVertices.containsKey(vertex.getId())) {
 			return;
@@ -29,6 +37,12 @@ public class DirectedGraph<T> implements Cloneable {
 		allVertices.put(vertex.getId(), vertex);
 	}
 
+	/**
+	 * Create a Vertex with the given id and add it to the Graph
+	 * 
+	 * @param id to create the Vertex with
+	 * @return created Vertex
+	 */
 	public Vertex<T> addSingleVertex(int id) {
 		if (allVertices.containsKey(id)) {
 			return allVertices.get(id);
@@ -38,6 +52,12 @@ public class DirectedGraph<T> implements Cloneable {
 		return v;
 	}
 
+	/**
+	 * Sets the data of a Vertex by id if it exists
+	 * 
+	 * @param id   to search for
+	 * @param data to set
+	 */
 	public void setDataForVertex(int id, T data) {
 		if (allVertices.containsKey(id)) {
 			Vertex<T> vertex = allVertices.get(id);
@@ -74,6 +94,13 @@ public class DirectedGraph<T> implements Cloneable {
 		return allVertices.size();
 	}
 
+	/**
+	 * Adds an Edge from id to id and creates the corresponding Vertices if they
+	 * don't exist yet
+	 * 
+	 * @param fromId source Vertex id
+	 * @param toId   destination Vertex id
+	 */
 	public void addEdge(int fromId, int toId) {
 		Vertex<T> fromVertex = null;
 		if (allVertices.containsKey(fromId)) {
@@ -92,14 +119,34 @@ public class DirectedGraph<T> implements Cloneable {
 		fromVertex.append(toVertex);
 	}
 
+	/**
+	 * Adds the Vertices if they don't exist yet and creates an Edge between them
+	 * 
+	 * @param from
+	 * @param to
+	 */
+	public void addEdge(Vertex<T> from, Vertex<T> to) {
+		addVertex(from);
+		addVertex(to);
+		addEdge(from.getId(), to.getId());
+	}
+
+	/**
+	 * Adds an Edge and inserts the corresponding Vertices if they
+	 * don't exist yet
+	 * 
+	 * @param edge to add
+	 */
 	public void addEdge(Edge<T> edge) {
 		addEdge(edge.getFrom(), edge.getTo());
 	}
 
-	public void addEdge(Vertex<T> from, Vertex<T> to) {
-		addEdge(from.getId(), to.getId());
-	}
-
+	/**
+	 * Removes the Edge in between the two Vertices if possible
+	 * 
+	 * @param fromId
+	 * @param toId
+	 */
 	public void removeEdge(int fromId, int toId) {
 		if (allVertices.containsKey(fromId)) {
 			allVertices.get(fromId).remove(toId);
@@ -114,9 +161,9 @@ public class DirectedGraph<T> implements Cloneable {
 	}
 
 	/**
-	 * Creates a deep copy of all Vertices and reconnects them
+	 * Creates a deep copy of the Graph
 	 * 
-	 * @return deep copy of the Graph
+	 * @return copied Graph
 	 */
 	public DirectedGraph<T> clone() {
 
@@ -138,20 +185,29 @@ public class DirectedGraph<T> implements Cloneable {
 		return graphCopy;
 	}
 
-	// David (29.12.2020)
+	/**
+	 * Deep copy the Graph and reverse all edges
+	 * 
+	 * @return reversed Graph
+	 */
 	public DirectedGraph<T> getReversed() {
 		DirectedGraph<T> outputGraph = new DirectedGraph<T>();
 		List<Edge<T>> edges = getAllEdges();
 		for (int i = 0; i != edges.size(); i++) {
 			Edge<T> edge = edges.get(i);
-			Vertex<T> vertex1 = edge.getFrom();
-			Vertex<T> vertex2 = edge.getTo();
+			Vertex<T> vertex1 = edge.getFrom().cloneWithoutEdges();
+			Vertex<T> vertex2 = edge.getTo().cloneWithoutEdges();
 			outputGraph.addEdge(vertex2, vertex1);
 		}
 		return outputGraph;
 	}
 
-	// David (29.12.2020)
+	/**
+	 * Get a deep copy of the Graph, with the specified IDs
+	 * 
+	 * @param idsToKeep
+	 * @return Subgraph
+	 */
 	public DirectedGraph<T> getSubGraph(Set<Integer> idsToKeep) {
 
 		// clone graph
@@ -167,33 +223,40 @@ public class DirectedGraph<T> implements Cloneable {
 	}
 
 	/**
-	 * Creates a SubGraph containing all the Vertices with an ID higher than
+	 * Creates a Subgraph containing all the Vertices with an ID higher than
 	 * minVertexId
 	 * 
-	 * @param minVertexId minimum id to look for
-	 * @return SubGraph
+	 * @param minVertexId minimum ID to look for
+	 * @return Subgraph
 	 */
 	public DirectedGraph<T> getSubGraph(int minVertexId) {
 
-		// create empty subgraph
-		DirectedGraph<T> subGraph = new DirectedGraph<>();
+		Set<Integer> idsToKeep = getAllVertices()
+				.stream()
+				.map(vertex -> vertex.getId())
+				.filter(id -> id >= minVertexId)
+				.collect(Collectors.toSet());
 
-		// add all Edges to the graph with ids >= minVertexId
-		for (Edge<T> edge : this.getAllEdges()) {
-			if (edge.getFrom().getId() >= minVertexId && edge.getTo().getId() >= minVertexId) {
-				subGraph.addEdge(edge.getFrom().getId(), edge.getTo().getId());
-			}
-		}
+		return getSubGraph(idsToKeep);
 
-		return subGraph;
 	}
 
 	@Override
 	public String toString() {
 		return allVertices.values().toString() + "\n"
-				+ String.join("\n", getAllEdges().stream().map(edge -> edge.toString()).collect(Collectors.toList()));
+				+ String.join("\n", getAllEdges()
+						.stream()
+						.map(edge -> edge.toString())
+						.collect(Collectors.toList()));
 	}
 
+	/**
+	 * Checks this Graph equals another Directed Graph
+	 * 
+	 * @param otherGraph to compare to
+	 * @return true if there is the same amount of Edges, Vertices and the IDs are
+	 *         equal
+	 */
 	public boolean equals(DirectedGraph<T> otherGraph) {
 		if (this.getVertexCount() != otherGraph.getVertexCount())
 			return false;
@@ -212,7 +275,15 @@ public class DirectedGraph<T> implements Cloneable {
 		return true;
 	}
 
-	public static DirectedGraph<Integer> fromString(String edgeString) {
+	/**
+	 * Creates a Graph form a String containing Edges<br>
+	 * Format: {fromId, toId}<br>
+	 * Separator: any
+	 * 
+	 * @param edgesString
+	 * @return DirectedGraph
+	 */
+	public static DirectedGraph<Integer> fromString(String edgesString) {
 
 		// edge string pattern
 		final Pattern pattern = Pattern.compile(
@@ -220,7 +291,7 @@ public class DirectedGraph<T> implements Cloneable {
 				Pattern.CASE_INSENSITIVE);
 
 		// match the pattern in the file
-		Matcher matcher = pattern.matcher(edgeString);
+		Matcher matcher = pattern.matcher(edgesString);
 
 		// new directed graph
 		DirectedGraph<Integer> graph = new DirectedGraph<>();
@@ -248,14 +319,30 @@ public class DirectedGraph<T> implements Cloneable {
 		return graph;
 	}
 
-	/// loads graph from file
+	/**
+	 * Creates a Directed Graph from a file
+	 * 
+	 * @param filename
+	 * 
+	 * @return Directed Graph
+	 * 
+	 * @throws InvalidPathException - if the filename string cannot be converted to
+	 *                              a Path
+	 * @throws IOException          - if an I/O error occurs reading from the file
+	 *                              or a malformed or unmappable byte sequence is
+	 *                              read
+	 * @throws SecurityException    - In the case of the default provider, and a
+	 *                              security manager is installed, the checkRead
+	 *                              method is invoked to check read access to the
+	 *                              file.
+	 */
 	public static DirectedGraph<Integer> fromFile(String filename)
 			throws InvalidPathException, IOException, SecurityException {
 
 		Path path = Paths.get(filename);
 
 		// concatenate all lines
-		String line = String.join(" ", Files.readAllLines(path));
+		String line = String.join("\n", Files.readAllLines(path));
 
 		return fromString(line);
 
