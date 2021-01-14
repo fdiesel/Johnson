24789e38e188409673e93a059522e12026931b72
Florian Diesel <florian.diesel@posteo.de>
2021-01-14 17:04:40 +0100

    Split methods, optimized string concatenation with buffer

--- src/main/java/main/ConsoleTable.java
+++ src/main/java/main/ConsoleTable.java
@@ -3,35 +3,38 @@ package main;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public class ConsoleTable {
-	
+
 	public enum Align {
+
 		Left, Right;
+
 		public int value() {
-			if (this == Align.Left) return -1;
-			else return 1;
+			return this == Align.Left ? -1 : 1;
 		}
+
 	}
 
-	private Align[] alignments;
-	private List<String[]> titleRows = new ArrayList<>();
+	private final Align[] alignments;
+	private List<String[]> headerRows = new ArrayList<>();
 	private List<String[]> bodyRows = new ArrayList<>();
 	private int[] colWidths;
-	
+
 	public ConsoleTable(Align... alignments) {
 		this.alignments = alignments;
 		colWidths = new int[this.alignments.length];
 	}
-	
+
 	public void addTitleRow(String... row) {
-		this.addRow(this.titleRows, row);
+		this.addRow(this.headerRows, row);
 	}
-	
-	public void addBodyRow(String... row) {
+
+	public void addHeaderRow(String... row) {
 		this.addRow(this.bodyRows, row);
 	}
-	
+
 	private void addRow(List<String[]> target, String[] row) {
 		if (row.length != this.colWidths.length) {
 			System.err.println("Number of cells doesn't match the tables number of columns");
@@ -40,32 +43,67 @@ public class ConsoleTable {
 		}
 		target.add(row);
 		for (int i = 0; i < this.colWidths.length; i++) {
-			if (this.colWidths[i] < row[i].length()) this.colWidths[i] = row[i].length();
+			if (this.colWidths[i] < row[i].length())
+				this.colWidths[i] = row[i].length();
 		}
 	}
-	
+
+	private String generateSeparator() {
+		StringBuffer buffer = new StringBuffer();
+		for (int width : colWidths)
+			buffer.append("+-" + "-".repeat(width) + "-");
+		buffer.append("+");
+		return buffer.toString();
+	}
+
+	private String generateFormat() {
+		return generateFormat(Optional.empty());
+	}
+
+	private String generateFormat(Align forcedAlign) {
+		return generateFormat(Optional.of(forcedAlign));
+	}
+
+	private String generateFormat(Optional<Align> optForcedAlign) {
+		StringBuffer buffer = new StringBuffer();
+		for (int i = 0; i < this.colWidths.length; i++) {
+			Align alignment = optForcedAlign.orElse(this.alignments[i]);
+			int stringModifier = alignment.value();
+			buffer.append("| %" + (this.colWidths[i] * stringModifier) + "s ");
+		}
+		buffer.append("|\n");
+		return buffer.toString();
+	}
+
 	@Override
 	public String toString() {
-		String spacer = "";
-		String titleFormat = "";
-		String bodyFormat = "";
-		for (int i = 0; i < this.colWidths.length; i++) {
-			spacer += "+-" + "-".repeat(this.colWidths[i]) + "-";
-			titleFormat += "| %-" + this.colWidths[i] + "s ";
-			bodyFormat += "| %" + (this.colWidths[i] * this.alignments[i].value()) + "s ";
-		}
-		spacer += "+";
-		titleFormat += "|\n";
-		bodyFormat += "|\n";
-		String out = spacer + "\n";
-		for (String[] row : this.titleRows) out += String.format(titleFormat, (Object[]) row);
-		if (this.titleRows.size() > 0 && this.bodyRows.size() > 0) out += spacer + "\n";
-		for (String[] row : this.bodyRows) out += String.format(bodyFormat, (Object[]) row);
-		out += spacer;
-		return out;
+
+		String separator = generateSeparator();
+		String headerFormat = generateFormat(Align.Left);
+		String bodyFormat = generateFormat();
+
+		StringBuffer result = new StringBuffer();
+		result.append(separator + "\n");
+
+		// append header
+		for (String[] row : this.headerRows)
+			result.append(String.format(headerFormat, (Object[]) row));
+
+		// separate header and body if both exist
+		if (this.headerRows.size() > 0 && this.bodyRows.size() > 0)
+			result.append(separator + "\n");
+
+		// append body
+		for (String[] row : this.bodyRows)
+			result.append(String.format(bodyFormat, (Object[]) row));
+
+		result.append(separator);
+
+		return result.toString();
+
 	}
-	
-	public void println() {
+
+	public void display() {
 		System.out.println(this.toString());
 	}
 
